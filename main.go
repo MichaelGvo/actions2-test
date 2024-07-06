c4fc9028285cf25b91b90fd59512cebd225ec732
MichaelGvo <161063068+MichaelGvo@users.noreply.github.com>
2024-07-06 16:50:33 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
     var wg sync.WaitGroup
     wg.Add(5)
 
-    for i := 0; i < 100000; i++ {
+    for i := 0; i < 5; i++ {
         go func() {
             fmt.Println(i)
             wg.Done()
